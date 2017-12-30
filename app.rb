80d6216e4ce37b741fca96ef91be87b4da005ba1
Lara Sanders <larasanders@gmail.com>
2017-12-30 16:11:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,21 +7,17 @@ class App < Sinatra::Base
   end
 
   get '/' do
-<<<<<<< HEAD
-=======
+
       @secret = params[:item], params[:name]
->>>>>>> 72c697e1dfe58eaf1e04ca008c9b069082ac2929
+
 
     erb :index
   end
 
   post '/checkout' do
-<<<<<<< HEAD
+
     @session = session
     session[:item] = params[:item]
-
-=======
->>>>>>> 72c697e1dfe58eaf1e04ca008c9b069082ac2929
 
     erb :checkout
   end
