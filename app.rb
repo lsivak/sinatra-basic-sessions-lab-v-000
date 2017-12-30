9c65aa6b81a06c714f2d4dff1552b67a7265ecfd
Lara Sanders <larasanders@gmail.com>
2017-12-30 02:31:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
+  configure do
+    enable :sessions unless test?
+    set :session_secret, "secret"
+  end
 
   get '/' do
 
     erb :index
+  end
+
+  post '/checkout' do
+    @session = session
+    session[:item] = params[:item]
+
+
+    erb :checkout
   end
 end
